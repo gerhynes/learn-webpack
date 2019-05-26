8a8073097db441a76b7fd6f7efc5d0a747a7e2d2
Gerard Hynes <gkhynes@gmail.com>
2019-05-26 23:36:36 +0100

    Split development and production

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,7 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.[contentHash].js",
-    path: path.resolve(__dirname, "dist")
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html"
